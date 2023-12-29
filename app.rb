436c4bfa92be143b011c8e71d1c7b7d521b4aacb
Brian Kim <brianhkim13@gmail.com>
2023-12-29 21:04:52 +0000

    navbar

--- app.rb
+++ app.rb
@@ -7,5 +7,38 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
+  erb :navbar
   erb :elephants
+end
+
+require 'sinatra'
+
+def play_rock_paper_scissors(player_choice)
+  choices = ["Rock", "Paper", "Scissors"]
+  computer_choice = choices.sample
+
+  result = ""
+
+  case player_choice
+  when "Rock"
+    case computer_choice
+    when "Rock"
+      result = "We tie"
+    when "Paper"
+      result = "We lose"
+    when "Scissors"
+      result = "We win"
+    end
+  else
+    result = "Invalid choice. Please choose Rock, Paper, or Scissors."
+  end
+
+  "Player chooses: #{player_choice}<br>Computer chooses: #{computer_choice}<br>Result: #{result}"
+end
+
+get '/rock' do
+  erb :navbar
+  player_choice = "Rock"
+  play_rock_paper_scissors(player_choice)
+
 end
